abee1d3ae24eac67d16cf0d6a8873b7edb8432eb
wangxihong <xih.wang@sunyard.com>
2020-06-27 19:01:35 +0800

    springcloud 配置中心学习项目上传

---
+++ configs/src/test/java/com/wxh/configs/ConfigsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxh.configs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ hello-world/src/main/java/com/wxh/helloworld/HelloWorldApplication.java
@@ -0,0 +1,15 @@
+package com.wxh.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+//@EnableEurekaClient
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
